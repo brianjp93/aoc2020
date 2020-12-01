f03b5ed3683d141bf084cc4d9568a1a56901e54d
Brian Perrett <bperrett@fusionbox.com>
2020-12-01 14:36:13 -0700

    Recursive general solution for part 1.

---
+++ python/day01/day1.py
@@ -0,0 +1,26 @@
+import pathlib
+from math import prod
+
+CWD = pathlib.Path(__file__).parent.absolute()
+filename = pathlib.PurePath(CWD, 'data')
+with open(filename) as f:
+    nums = set(int(x.strip()) for x in f.readlines())
+
+def find_group(nums, n, count):
+    if n < 0:
+        return None
+    for i in nums:
+        nums.remove(i)
+        if count <= 2:
+            if n-i in nums:
+                return [i, n-i]
+        else:
+            possible = find_group(nums, n-i, count-1)
+            if possible:
+                return [i] + possible
+        nums.add(i)
+
+group = find_group(nums, 2020, 2)
+print(f'Part 1: {prod(group)}')
+group = find_group(nums, 2020, 3)
+print(f'Part 2: {prod(group)}')

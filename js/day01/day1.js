c82f667e85f6e216e8f9bfe0df5d7e694e40843d
Brian Perrett <bperrett@fusionbox.com>
2020-12-01 17:20:38 -0700

    Read file synchronously.

--- js/day01/day1.js
+++ js/day01/day1.js
@@ -3,13 +3,12 @@ import path from 'path'
 
 let filename = path.join(path.resolve(), 'data')
 
-fs.readFile(filename, 'utf8', (err, data) => {
-    data = new Set(data.split('\n').filter(x => x !== '').map(item => {
-        return parseInt(item.trim())
-    }))
-    console.log(find_group(data, 2020, 2))
-    console.log(find_group(data, 2020, 3))
-})
+let data = new Set(
+    fs.readFileSync(filename, 'utf8')
+        .split('\n')
+        .filter(x => x !== '')
+        .map(item => parseInt(item.trim()))
+)
 
 function find_group(nums, n, count) {
     if (n < 0) {
@@ -33,3 +32,6 @@ function find_group(nums, n, count) {
     }
     return null
 }
+
+console.log(find_group(data, 2020, 2))
+console.log(find_group(data, 2020, 3))

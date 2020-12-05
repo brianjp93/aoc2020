81d87c7c117033488008023f2fec0f1b3ca44372
= <bperrett@fusionbox.com>
2020-12-05 02:17:48 -0700

    I realized these are just binary numbers

--- python/day05/day5.py
+++ python/day05/day5.py
@@ -5,27 +5,10 @@ CWD = pathlib.Path(__file__).parent.absolute()
 filename = pathlib.PurePath(CWD, 'data')
 
 def find_seat(word):
-    part1 = word[:7]
-    part2 = word[7:]
-    start = 1
-    end = 128
-    for ch in part1:
-        size = end - start + 1
-        if ch == 'F':
-            end  -= size // 2
-        elif ch == 'B':
-            start += size // 2
-    row = start - 1
-
-    start = 1
-    end = 8
-    for ch in part2:
-        size = end - start + 1
-        if ch == 'L':
-            end  -= size // 2
-        elif ch == 'R':
-            start += size // 2
-    col = start - 1
+    word = word.replace('B', '1').replace('L', '0')
+    word = word.replace('F', '0').replace('R', '1')
+    row = int(word[:7], 2)
+    col = int(word[7:], 2)
     return row, col
 
 def find_my_seat(seat_ids):

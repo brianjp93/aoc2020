9b73e8bf530c746201bce5dc5a079fd3d32efdfe
= <bperrett@fusionbox.com>
2020-12-06 05:17:10 -0700

    Don't need to specify a set to begin with

--- python/day06/day6.py
+++ python/day06/day6.py
@@ -10,8 +10,8 @@ inters = []
 unions = []
 for group in data:
     groupsets = [set(ch for ch in person) for person in group.splitlines()]
-    inters.append(groupsets[0].intersection(*groupsets))
-    unions.append(groupsets[0].union(*groupsets))
+    inters.append(set.intersection(*groupsets))
+    unions.append(set.union(*groupsets))
 
 print(f'Part 1: {sum(len(x) for x in unions)}')
 print(f'Part 2: {sum(len(x) for x in inters)}')

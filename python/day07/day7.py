5d89e0e1c6efd777dec058d7653d067428cfcda9
Brian Perrett <bperrett@fusionbox.com>
2020-12-07 11:22:08 -0700

    Clean up parsing and use memo decorator

--- python/day07/day7.py
+++ python/day07/day7.py
@@ -6,62 +6,50 @@ filename = pathlib.PurePath(CWD, 'data')
 with open(filename) as f:
     data = [x.strip() for x in f.read().strip().split('\n')]
 
-
+LEFT_REGEX = re.compile(r'(\w+ \w+) bags contain')
+RIGHT_REGEX = re.compile(r'(\d+) (\w+ \w+)')
 bags = {}
 for line in data:
-    bag, others = line.split('bags contain')
-    bag = bag.strip()
-    bags[bag] = {}
-    others = others.strip()
-    for otherbag in others.split(','):
-        otherbag = otherbag.strip().strip('bags.').strip('bag.').strip()
-        if 'no other' not in otherbag:
-            bagsplit = otherbag.split(' ')
-            num = bagsplit[0]
-            bagtype = bagsplit[1:]
-            bagtype = ' '.join(bagtype)
-            bags[bag][bagtype] = int(num)
+    bag = LEFT_REGEX.search(line).groups()[0]
+    right = RIGHT_REGEX.findall(line)
+    bags[bag] = {key: int(val) for val, key in right}
 
-cache = {}
-count_inner = {}
+def memo(f):
+    mem = {}
+    def inner(*args):
+        arg_string = '-'.join([f'{i},{arg}' for i, arg in enumerate(args)])
+        key = f'{__file__}-{f.__name__}-{arg_string}'
+        if key not in mem:
+            mem[key] = f(*args)
+        return mem[key]
+    return inner
+
 def has_bag(bagtype):
     count = 0
-    for bag, innerbags in bags.items():
+    for bag in bags:
         if check_bag(bag, bagtype):
             count += 1
     return count
 
+@memo
 def check_bag(bag, bagtype):
+    out = False
     for inner in bags[bag]:
         if inner == bagtype:
-            cache[bag] = True
-            return True
-        if cache.get(inner) is True:
-            cache[bag] = True
-            return True
-        elif cache.get(inner) is False:
-            continue
+            out = True
         elif check_bag(inner, bagtype):
-            cache[bag] = True
-            return True
-    cache[bag] = False
-    return False
+            out = True
+    return out
 
-def count_bags(bag):
-    if len(bags[bag]) == 0:
-        return 1
-    else:
-        total = 1
-        for inner, count in bags[bag].items():
-            if inner in count_inner:
-                total += (count * count_inner[inner])
-            else:
-                total += (count * count_bags(inner))
-        count_inner[bag] = total
-        return total
+@memo
+def _count_bags(bag):
+    total = 1
+    for inner, count in bags[bag].items():
+        mult = _count_bags(inner)
+        total += count * _count_bags(inner)
+    return total
+def count_bags(*args):
+    return _count_bags(*args) - 1
 
-
-count = has_bag('shiny gold')
-print(count)
-
-print(count_bags('shiny gold') - 1)
+print(f'Part 1: {has_bag("shiny gold")}')
+print(f'Part 2: {count_bags("shiny gold")}')

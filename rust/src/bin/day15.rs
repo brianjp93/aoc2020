6d6987e1a2ade611c369139e3b0d4aaf92ccac83
Brian Perrett <bperrett@fusionbox.com>
2020-12-15 15:33:57 -0700

    Start day 15 in rust

---
+++ rust/src/bin/day15.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+type N = i128;
+type NList = Vec<i128>;
+
+fn main() {
+    let d: NList = vec![17, 1, 3, 16, 19, 0];
+
+    for n in &[2020, 3] {
+        let mut dc = d.clone();
+        let mut history: HashMap<N, NList> = HashMap::new();
+        let mut i = 0_i128;
+        let mut speak: i128;
+        while &i < n {
+        }
+    }
+}

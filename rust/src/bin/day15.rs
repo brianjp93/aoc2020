f1ec34086c1d282b49ec0dd2e154820e6af29e3d
Brian Perrett <bperrett@fusionbox.com>
2020-12-15 16:18:53 -0700

    day15 rust

--- rust/src/bin/day15.rs
+++ rust/src/bin/day15.rs
@@ -5,13 +5,40 @@ type NList = Vec<i128>;
 
 fn main() {
     let d: NList = vec![17, 1, 3, 16, 19, 0];
+    let init_length = d.len() as usize;
 
-    for n in &[2020, 3] {
-        let mut dc = d.clone();
+    for n in &[2020, 30000000] {
+        let dc = d.clone();
         let mut history: HashMap<N, NList> = HashMap::new();
-        let mut i = 0_i128;
-        let mut speak: i128;
+        let mut i = 0 as usize;
+        let mut speak: N = -1;
         while &i < n {
+            if i < init_length {
+                speak = dc[i];
+            }
+            else if let Some(x) = history.get_mut(&speak) {
+                if x.len() > 1 {
+                    speak = x[x.len()-1] - x[x.len()-2];
+                }
+                else {
+                    speak = 0;
+                }
+            }
+            else {
+                speak = 0;
+            }
+
+            if let Some(x) = history.get_mut(&speak) {
+                x.push((i + 1) as N);
+                if x.len() > 2 {
+                    x.drain(0..1);
+                }
+            }
+            else {
+                history.insert(speak, vec![(i+1) as N]);
+            }
+            i += 1;
         }
+        println!("{:?}", speak)
     }
 }

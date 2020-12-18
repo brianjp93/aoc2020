f3a2c17c39dda956afbf8b16d6a7bee9d9fd224c
= <bperrett@fusionbox.com>
2020-12-18 14:21:19 -0700

    Shorter

--- python/day18/day18.py
+++ python/day18/day18.py
@@ -16,33 +16,19 @@ def evaluate(line):
 
 def evaluate2(line):
     while True:
-        while True:
-            _line = line
-            line = re.sub(r'(\d+\+\d+)', replace, line)
-            if line == _line:
-                break
-        while True:
-            _line = line
-            line = re.sub(r'[\(\*](\d+\*\d+)[\)\*]', replace, line)
-            if line == _line:
-                break
+        _line = line
+        line = re.sub(r'(\d+\+\d+)', replace, line)
+        line = re.sub(r'[\(\*](\d+\*\d+)[\)\*]', replace, line)
         line = re.sub(r'(\(\d+\))', lambda x: x.group(0)[1:-1], line)
         if '+' not in line or '*' not in line:
             return eval(line)
-    return line
 
 def replace(match_obj):
     part = match_obj.group(0)
     return part.replace(match_obj.group(1), str(eval(match_obj.group(1))))
 
 
-total = 0
-total2 = 0
-for line in d:
-    line = ''.join(line.strip().split())
-    out = evaluate(line)
-    out2 = evaluate2(line)
-    total += out
-    total2 += out2
-print(f'Part 1: {total}')
+total1 = sum(evaluate(''.join(line.strip().split())) for line in d)
+total2 = sum(evaluate2(''.join(line.strip().split())) for line in d)
+print(f'Part 1: {total1}')
 print(f'Part 2: {total2}')

71638896826010e1ce3a103743e7156a92ef4ec2
= <bperrett@fusionbox.com>
2020-12-27 05:26:02 -0700

    day 19 yey

--- python/day19/day19.py
+++ python/day19/day19.py
@@ -1,8 +1,9 @@
 import pathlib
 import re
 from string import ascii_lowercase
+import copy
 
-filename = pathlib.PurePath(pathlib.Path(__file__).parent.absolute(), 'test')
+filename = pathlib.PurePath(pathlib.Path(__file__).parent.absolute(), 'data')
 with open(filename) as f:
     lines = [x.strip() for x in f.read().strip().split('\n')]
     rules = {}
@@ -22,17 +23,37 @@ with open(filename) as f:
                 words.append(line)
 
 
-def get_rule(n):
+def get_rule(n, rules,  init=True):
     if isinstance(rules[n][0], str):
-        return [rules[n][0]]
+        return rules[n][0]
     out = []
-    for group in rules[n]:
-        part = []
-        for x in group:
-            part = part + get_rule(x)
-        out.append(part)
-    return out
+    for part in rules[n]:
+        temp = []
+        for x in part:
+            temp.append(f'({get_rule(x, rules, init=False)})')
+        if temp:
+            out.append(''.join(temp))
+    if out:
+        if init:
+            return f'^{"|".join(out)}$'
+        return '|'.join(out)
+    return ''
 
-print(get_rule(0))
-# print(words)
-# print(rules)
+rule = re.compile(get_rule(0, rules))
+out = sum(rule.match(word) is not None for word in words)
+print(f'Part 1: {out}')
+
+rule_8 = get_rule(8, rules, init=False)
+rule_11 = get_rule(11, rules, init=False)
+rule_42 = get_rule(42, rules, init=False)
+rule_31 = get_rule(31, rules, init=False)
+
+total = 0
+for i in range(5):
+    rule_11_repeat = f'({rule_42}){{{i}}}{rule_11}({rule_31}){{{i}}}'
+    rule = re.compile(f'^{rule_8}+{rule_11_repeat}$')
+    for word in words:
+        if rule.match(word):
+            total += 1
+
+print(f'Part 2: {total}')
